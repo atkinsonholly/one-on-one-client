60eb015821075b953c209b44febab67a3b31202b
atkinsonholly <atkinsonholly@gmail.com>
2025-02-08 16:22:05 +0000

    add testnet contract address and function name

--- src/components/atoms/NFT.tsx
+++ src/components/atoms/NFT.tsx
@@ -18,16 +18,20 @@ const NFT: React.FC<NFTProps> = ({ balance, id }) => {
             {balance == "0" ? 
             <Button
             color="blue" bg="green" fontSize="18px" fontFamily="alt" width='260px'
-            // onClick={() => {
-            //     writeContract({ 
-            //         nftAbi,
-            //         address: '0x',
-            //         functionName: 'mint',
-            //         args: [
-            //           userAddress
-            //         ],
-            //      })
-            // }}
+            onClick={() => {
+                if (userAddress) {
+                  writeContract({ 
+                      abi: nftAbi,
+                      address: '0x65725931BF9d37d7e1b1CEb90928271B572829F4', // base_sepolia
+                      functionName: 'mintWithETH',
+                      args: [
+                        userAddress
+                      ],
+                   })
+                } else {
+                  console.error('User address is undefined');
+                }
+            }}
             >
             Mint NFT
             </Button> : <Box><Image src='OneOnOne_square.webp' width="400px" /></Box>} 
